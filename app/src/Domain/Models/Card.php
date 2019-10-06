d87338c3b1f0ae2b3532e48fcbfc6ad824510b3a
tig <example@example.com>
2019-10-06 16:00:29 +0900

    update

--- app/src/Domain/Models/Card.php
+++ app/src/Domain/Models/Card.php
@@ -2,7 +2,7 @@
 
 namespace TKuni\AnkiCardGenerator\Domain\Models;
 
-class Card
+class Card implements \JsonSerializable
 {
     /**
      * @var string
@@ -16,19 +16,36 @@ class Card
     public function __construct(string $front, string $back)
     {
         $this->front = $front;
-        $this->back = $back;
+        $this->back  = $back;
     }
 
-    public function front() {
+    public function front()
+    {
         return $this->front;
     }
 
-    public function back() {
+    public function back()
+    {
         return $this->back;
     }
 
     public function __debugInfo()
     {
         return (string)((array)$this);
+    }
+
+    /**
+     * Specify data which should be serialized to JSON
+     * @link https://php.net/manual/en/jsonserializable.jsonserialize.php
+     * @return mixed data which can be serialized by <b>json_encode</b>,
+     * which is a value of any type other than a resource.
+     * @since 5.4.0
+     */
+    public function jsonSerialize()
+    {
+        return [
+            'front' => $this->front,
+            'back'  => $this->back
+        ];
     }
 }
